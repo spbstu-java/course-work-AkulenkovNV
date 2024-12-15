ff408737049fdce37811fbc25b603381b372a6a6
AkulenkovNV <paskalkortez@gmail.com>
2024-12-15 22:30:16 +0300

    Delete demo1 directory

--- demo1/src/main/java/com/example/demo1/l1/MoveStrategy.java
+++
@@ -1,8 +0,0 @@
-package com.example.demo1.l1;
-
-interface MoveStrategy {
-    void move(int x, int y, Hero hero);
-
-    public String getName();
-}
-
--- demo1/src/main/java/com/example/demo1/l3/InvalidFileFormatException.java
+++
@@ -1,8 +0,0 @@
-package com.example.demo1.l3;
-public class InvalidFileFormatException extends Exception {
-    public InvalidFileFormatException(String message) {
-        super(message);
-    }
-}
-
-
